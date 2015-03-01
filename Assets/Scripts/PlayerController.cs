a437f5c1d12cf275c5524a5c171b3ed7f1dccadc
Peter Corazao <pcorazao@pcorazao.local>
2015-03-01 16:21:59 -0600

    adding my first game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	private int count;
+
+	public UnityEngine.UI.Text text;
+	public UnityEngine.UI.Text winText;
+
+
+	void Start()
+	{
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp") 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText();		}
+	}
+
+	void SetCountText()
+	{
+		text.text = "Count: " + count.ToString ();
+		if (count == 6) {
+			winText.text = "Win!";
+		}
+	}
+}
